a20bb64f3f2a0cc2e0e1e1b718311934f3fb5402
Tung Huynh <htqtung@gmail.com>
2019-10-11 09:43:30 +0300

    added seach function

---
+++ src/components/App.js
@@ -0,0 +1,99 @@
+import React, { useReducer, useEffect } from 'react';
+import '../App.css';
+import Header from './Header';
+import Search from './Search';
+import Movie from './Movie';
+
+const MOVIE_API_URL = "http://www.omdbapi.com/?s=man&apikey=41a0fe80"
+
+const initialState = {
+  loading: true,
+  movies: [],
+  errorMessage: null
+};
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "SEARCH_MOVIES_REQUEST":
+      return {
+        ...state,
+        loading: true,
+        errorMessage: null
+      };
+    case "SEARCH_MOVIES_SUCCESS":
+      return {
+        ...state,
+        loading: false,
+        movies: action.payload
+      };
+    case "SEARCH_MOVIES_FAILURE":
+      return {
+        ...state,
+        loading: false,
+        errorMessage: action.error
+      };
+  
+    default:
+      return state;
+  }
+}
+
+const App = () => {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  useEffect(() => {
+    fetch(MOVIE_API_URL)
+      .then(res => res.json())
+      .then(jsonRes => {
+        dispatch({
+          type: "SEARCH_MOVIES_SUCCESS",
+          payload: jsonRes.Search
+        });
+      });
+  }, []);
+
+  const search = searchValue => {
+    dispatch({
+      type: "SEARCH_MOVIES_REQUEST"
+    });
+
+    fetch(`http://www.omdbapi.com/?s=${searchValue}&apikey=41a0fe80`)
+      .then(res => res.json())
+      .then(jsonRes => {
+        if(jsonRes.Response === "True") {
+          dispatch({
+            type: "SEARCH_MOVIES_SUCCESS",
+            payload: jsonRes.Search
+          });
+        } else {
+          dispatch({
+            type: "SEARCH_MOVIES_FAILURE",
+            error: jsonRes.Error
+          });
+        }
+      });
+  };
+
+  const { movies, errorMessage, loading } = state;
+
+  return (
+    <div className="App">
+      <Header text="MOVIEFINDER" />
+      <Search search={search} />
+      <p className="App-intro">Sharing a few of our favourite movies</p>
+      <div className="movies">
+        {loading && !errorMessage ? (
+          <span>loading...</span>
+        ) : errorMessage ? (
+          <div className="errorMessage">{errorMessage}</div>
+        ) : (
+          movies.map((movie, index) => (
+            <Movie key={`${index}-${movie.Title}`} movie={movie} />
+          ))
+        )}
+      </div>
+    </div>
+  );
+}
+
+export default App;
